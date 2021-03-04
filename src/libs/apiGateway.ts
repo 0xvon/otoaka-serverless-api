17f71bde7b84badcd190c621ffb846b74e04abcb
masatsch <kingmasatojames@gmail.com>
2021-03-04 22:25:30 +0900

    dosnt map jsonstringify

--- src/libs/apiGateway.ts
+++ src/libs/apiGateway.ts
@@ -5,15 +5,15 @@ type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { b
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
 export const formatJSONResponse = (response: Record<string, unknown>) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response)
-  }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+    }
 }
 
 export const formatHTMLResponse = (response: string) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response)
-  }
+    return {
+        statusCode: 200,
+        body: response
+    }
 }
