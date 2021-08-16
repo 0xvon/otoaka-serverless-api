c7361047e83e9fbbe42512fdb8c6b912e938053b
masatsch <kingmasatojames@gmail.com>
2021-08-16 10:29:27 +0900

    use stream

--- src/functions/clients/S3Client.ts
+++ src/functions/clients/S3Client.ts
@@ -13,6 +13,17 @@ export class S3Client {
     }
 
     upload = async (imageUrl: string, key: string) => {
+        var typeMatch = imageUrl.match(/\.([^.]*)$/);
+        if (!typeMatch) {
+            console.log("Could not determine the image type.");
+            return;
+        }
+        var imageType = typeMatch[1];
+        if (imageType != "jpg" && imageType != "jpeg" && imageType != "png") {
+            console.log(`Unsupported image type: ${imageType}`);
+            return;
+        }
+
         console.log(`uploading ${imageUrl} ...`);
 
         const imageRes = await axios.get(imageUrl, {responseType: 'arraybuffer'});
@@ -35,12 +46,23 @@ export class S3Client {
 
     resize = async (image: gm.State): Promise<Buffer> => {
         return new Promise((resolve, reject) => {
-            image.resize(400).toBuffer('jpeg', (err, buffer) => {
+            image.resize(400).stream('jpeg', (err, stdout, stderr) => {
                 if (err) {
+                    console.log('stream process error');
+                    console.log(err, stdout, stderr);
                     reject(err);
-                } else {
-                    resolve(buffer);
                 }
+
+                var chunks = [];
+                stdout.on('data', function(chunk) {
+                    console.log('pushed');
+                    chunks.push(chunk);
+                });
+                stdout.on('end', function() {
+                    console.log('end');
+                    var buffer = Buffer.concat(chunks);
+                    resolve(buffer);
+                });
             })
         });
     }
