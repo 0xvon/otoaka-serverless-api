cb17703e4f5e03e8be73f1aaa7f59e30cba3a179
masatsch <kingmasatojames@gmail.com>
2021-08-16 13:28:22 +0900

    use imagemagick

--- src/functions/clients/S3Client.ts
+++ src/functions/clients/S3Client.ts
@@ -1,7 +1,8 @@
 import * as AWS from 'aws-sdk';
 const axios = require('axios');
-import * as gm from 'gm';
-const im = gm.subClass({ imageMagick: true });
+// import * as gm from 'gm';
+// const im = gm.subClass({ imageMagick: true });
+const im = require('imagemagick');
 
 export class S3Client {
     s3: AWS.S3;
@@ -30,8 +31,8 @@ export class S3Client {
         const imageData: ArrayBuffer = imageRes.data;
         const imageBuffer = Buffer.from(imageData);
         console.log(`imageBuffer is undefined?: ${imageBuffer === undefined}`);
-        const image = im(imageBuffer);
-        const resized = await this.resize(image);
+        // const image = im(imageBuffer);
+        const resized = await this.resize(imageBuffer);
         console.log('resizing complete!', resized.toString());
 
         const res = await this.s3.putObject({
@@ -44,37 +45,51 @@ export class S3Client {
         return res
     }
 
-    resize = async (image: gm.State): Promise<Buffer> => {
+    // resize = async (image: gm.State): Promise<Buffer> => {
+    //     return new Promise((resolve, reject) => {
+    //         image
+    //             .resize(400, 400)
+    //             .setFormat('jpeg')
+    //             .toBuffer((err, buffer) => {
+    //                 if (err) { reject(err) }
+    //                 else { resolve(buffer) }
+    //             })
+
+    //             .stream((err, stdout, stderr) => {
+    //             if (err) {
+    //                 console.log('stream process error');
+    //                 console.log(err, stdout, stderr);
+    //                 reject(err);
+    //             }
+
+    //             var chunks = [];
+    //             stdout.on('data', function(chunk) {
+    //                 console.log('pushed');
+    //                 chunks.push(chunk);
+    //             });
+    //             stdout.on('end', function() {
+    //                 console.log('end');
+    //                 var buffer = Buffer.concat(chunks);
+    //                 resolve(buffer);
+    //             });
+
+    //             stderr.on('data',function(data) {
+    //                 console.log(`stderr data:`, data);
+    //             })
+    //         })
+    //     });
+    // }
+
+    resize = async (data: Buffer): Promise<Buffer> => {
         return new Promise((resolve, reject) => {
-            image
-                .resize(400, 400)
-                .toBuffer((err, buffer) => {
-                    if (err) { reject(err) }
-                    else { resolve(buffer) }
-                })
-
-                // .stream((err, stdout, stderr) => {
-                // if (err) {
-                //     console.log('stream process error');
-                //     console.log(err, stdout, stderr);
-                //     reject(err);
-                // }
-
-                // var chunks = [];
-                // stdout.on('data', function(chunk) {
-                //     console.log('pushed');
-                //     chunks.push(chunk);
-                // });
-                // stdout.on('end', function() {
-                //     console.log('end');
-                //     var buffer = Buffer.concat(chunks);
-                //     resolve(buffer);
-                // });
-
-                // stderr.on('data',function(data) {
-                //     console.log(`stderr data:`, data);
-                // })
-            // })
+            im.resize({
+                srcData: data,
+                format: "jpeg",
+                width: 400
+            }, (err, stdout, stderr) => {
+                if (err) { reject(err) }
+                else { resolve(Buffer.from(stdout, 'binary'))}
+            })
         });
     }
 }
